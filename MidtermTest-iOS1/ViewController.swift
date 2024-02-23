56c63a0afd63317ef1bac6ce1b579a7ad4fdc246
Anoop <33577002+Evans7@users.noreply.github.com>
2024-02-23 10:25:07 -0500

    Completed midterm

--- MidtermTest-iOS1/ViewController.swift
+++ MidtermTest-iOS1/ViewController.swift
@@ -7,12 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController{
-  
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
+    
+    var allTodos = [ToDo]()
+    var newlyAddedIndexPaths = Set<IndexPath>()
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return allTodos.count
+    }
+    @IBOutlet weak var urgentSwitch: UISwitch!
+    
+    @IBAction func isUrgent(_ sender: Any) {
+        
+    }
+    @IBOutlet weak var todoTable: UITableView!
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if(urgentSwitch.isOn)
+        {
+        
+            if(newlyAddedIndexPaths.contains(indexPath) )          {  cell.textLabel?.text = allTodos[indexPath.row].task
+                cell.textLabel?.textColor = UIColor.red
+                newlyAddedIndexPaths.remove(indexPath)
+                
+            }
+        }
+        else{
+            
+            if(newlyAddedIndexPaths.contains(indexPath))
+            {
+                cell.textLabel?.text = allTodos[indexPath.row].task
+                cell.textLabel?.textColor =
+                UIColor.green
+                newlyAddedIndexPaths.remove(indexPath)
+            }
+        }
+        
+        return cell
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+            
+        }
+    
     @IBOutlet weak var todoText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-     
+        urgentSwitch.isOn = false;
+
     }
 
 
@@ -21,7 +65,13 @@ class ViewController: UIViewController{
         if let todo_text = todoText.text {
             if  (!todo_text.isEmpty && todo_text.count > 0) {
                 let newToDo = ToDo(t: todo_text)
-               
+                let newIndexPath = IndexPath(row: allTodos.count, section: 0)
+                
+
+                allTodos.append(newToDo)
+                newlyAddedIndexPaths.insert(newIndexPath)
+                
+                todoTable.reloadData()
                 todoText.text = ""
             }
         }
